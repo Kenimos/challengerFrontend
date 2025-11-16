72b3ad2e16c93a82705680eee28929af77bd2c07
Jan Ječmínek <jan.jecminek12@gmail.com>
2025-11-17 00:00:53 +0100

    pwa2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,5 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
 export default function App() {
-    return <RouterProvider router={router} />;
+    return <div className="app-root">
+        <RouterProvider router={router} />
+    </div>;
 }
